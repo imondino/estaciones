08677369c9c9b40f763b2a79d8c463648aedbdd9
Ignacio Mondino <ignacio.mondino@gmail.com>
2018-04-29 21:26:16 -0300

    First commit

---
+++ app.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template
+import datetime
+import random
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+app = Flask(__name__)
+
+#Add some randomness
+now = datetime.datetime.now()
+random.seed(now)
+
+# list of cat subway stations
+stops = [
+    "Olleros",
+    "José Hernández",
+    "Juramento",
+    "Congreso de Tucumán"
+]
+
+@app.route('/')
+def index():
+
+    station = random.choice(stops)
+    print station
+    return render_template('index.html', station=station)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
